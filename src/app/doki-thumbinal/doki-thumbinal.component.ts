63df7579cce99924b27cd754aef26d2d87c32808
przemyslaw.sowinski <przemyslaw.sowinski@noreply.com>
2020-03-08 22:07:06 +0100

    thumbinal polyfills

--- src/app/doki-thumbinal/doki-thumbinal.component.ts
+++ src/app/doki-thumbinal/doki-thumbinal.component.ts
@@ -10,9 +10,18 @@ export class DokiThumbinalComponent implements OnInit {
 
   @Input() data: DokiSet;
 
+  visual_price: number;
+  visual_element_price: number;
+
   constructor() { }
 
   ngOnInit() {
+    this.calculate_price();
+  }
+
+  calculate_price() {
+    this.visual_price = this.data.price / 100;
+    this.visual_element_price = this.visual_price / this.data.elements;
   }
 
 }
