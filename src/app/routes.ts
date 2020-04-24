74c27e7fbdc844221544ff86ef7cf136f74aa709
przemyslaw.sowinski <przemyslaw.sowinski@noreply.com>
2020-04-24 23:59:21 +0200

    add user module with routing

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,3 +1,4 @@
+import { UserModule } from './user/user.module';
 import { DokiListResolver } from './services/doki-list.resolver';
 import { Routes } from '@angular/router';
 import { DokiDetailsComponent } from './doki-details/doki-details.component';
@@ -7,5 +8,6 @@ export const appRoutes: Routes = [
     { path: 'dokis', component: DokiListComponent, resolve: {dokidata: DokiListResolver} },
     { path: 'dokis/:id', component: DokiDetailsComponent },
     { path: 'dokiscategory/:id', component: DokiListComponent, resolve: {dokidata: DokiListResolver} },
-    { path: '', redirectTo: '/dokis', pathMatch: 'full' }
+    { path: '', redirectTo: '/dokis', pathMatch: 'full' },
+    { path: 'user', loadChildren: './user/user.module#UserModule'}
 ]
